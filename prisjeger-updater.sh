88950d6ad1776c7a3bce02436c657cfb25115113
DimseBoms <72513291+DimseBoms@users.noreply.github.com>
2022-11-06 18:19:14 +0100

    Fikset bug hvor feil /home ble brukt

--- prisjeger-updater.sh
+++ prisjeger-updater.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
- 
+
 # Ser etter oppdateringer på GitHub for så å oppdatere prosjektet automatisk
 # dersom oppdateringer blir funnet
 
 
 GIT_REPO="https://github.com/DimseBoms/prisjeger"
-GIT_DIRECTORY="$HOME/prisjeger"
-BACKUP_DIRECTORY="$HOME/backup"
+USER_HOME="/home/dmitriy"
+GIT_DIRECTORY="${USER_HOME}/prisjeger"
+BACKUP_DIRECTORY="${USER_HOME}/backup"
 UPDATE_INTERVAL=10
 
 
 function oppdaterProsjekt() {
-    systemctl stop prisjeger-backend.service
-    echo "$GIT_DIRECTORY finnes. Henter nyeste oppdateringer"
+    sudo systemctl stop prisjeger-backend.service
+    echo "Henter nyeste oppdateringer"
     rm -rf $BACKUP_DIRECTORY
     cp -rf $GIT_DIRECTORY $BACKUP_DIRECTORY
     cd $GIT_DIRECTORY
@@ -21,31 +22,37 @@ function oppdaterProsjekt() {
     git clean -fd
     git config pull.rebase true
     git pull
-    systemctl start prisjeger-backend.service
+    cd "${GIT_DIRECTORY}/backend"
+    npm i
+    # cd "${GIT_DIRECTORY}/frontend"
+    # npm i
+    sudo systemctl restart prisjeger-backend.service
+    sudo chown -R dmitriy:dmitriy $GIT_DIRECTORY
 }
 
 
 # Sjekker etter nye Git oppdateringer
 while true
 do
-    if [ -d "$GIT_DIRECTORY" ]; then
+    if [ -d "${GIT_DIRECTORY}" ]; then
+        # echo "${GIT_DIRECTORY} finnes"
         cd $GIT_DIRECTORY
         git fetch origin
             reslog=$(git log HEAD..origin/main --oneline)
             if [[ "${reslog}" != "" ]] ; then
                     echo "Git oppdatering funnet."
                     oppdaterProsjekt
-            else
-                    date=`date`
-                    echo "Ingen Git oppdateringer funnet. Sist sjekket: ${date}"
             fi
     else
-        echo "$GIT_DIRECTORY finnes ikke. Kloner nytt prosjekt"
-        mkdir -p $GIT_DIRECTORY
-        cd $HOME
+        echo "${GIT_DIRECTORY} finnes ikke. Kloner nytt prosjekt"
+        cd $USER_HOME
         git clone $GIT_REPO
-        cd $GIT_DIRECTORY
+        cd "${GIT_DIRECTORY}/backend"
+        npm i
+        # cd "${GIT_DIRECTORY}/frontend"
+        # npm i
+        sudo chown -R dmitriy:dmitriy $GIT_DIRECTORY
         systemctl restart prisjeger-backend.service
     fi
     sleep $UPDATE_INTERVAL
-done
+d
